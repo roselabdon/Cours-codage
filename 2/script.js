8507c044c22a92e3dc16d47f4b006ce89f4196f8
roselabdon <roselabdon@gmail.com>
2019-06-18 15:35:03 +0200

    etape 3 js

--- 2/script.js
+++ 2/script.js
@@ -1,8 +1,20 @@
 
 $( document ).ready(function() {
 	alert("Bienvenue");
-	$("p").addClass('red');
+	$("p").on( "click", function() {
+
+if ($( "p" ).hasClass('red')){
+	$( "p" ).removeClass('red');
+}else{
+	$( "p" ).addClass('red');
+	}
 });
+
+}); 
+
+
+
+
 
 
 
