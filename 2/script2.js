203a05033fc0dba32cd3b913d7dd9b63383a0323
roselabdon <roselabdon@gmail.com>
2019-07-02 09:24:31 +0200

    appel nom final

--- 2/script2.js
+++ 2/script2.js
@@ -1,20 +1,19 @@
-$( document ).ready(function() {
-  alert('Hello')
+$( document ).ready(function(){
 })
-function welcome() {
-    var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
-
-};
-// Appel la fonction
-welcome();
 
 function getUsername() {
     //affiche une boite de dialogue 
     var  user = prompt("Veuillez rentrez votre nom:", "");
     // la fonction se termine en retournant le nom récupéré par la boite de dialogue
     return user
-}
+};
+
+$( document ).ready(function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+});
+// Appel la fonction welcome();
 
 
 
