2c7aba775bc324d16274612f9a019a93702f5e92
roselabdon <roselabdon@gmail.com>
2019-07-02 08:56:15 +0200

    update script 2

--- 2/script2.js
+++ 2/script2.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
   alert('Hello')
 
-  function welcome() {
+function welcome() {
    $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
 };
-})
+welcome();
 
 
 
